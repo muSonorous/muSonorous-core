9db9d2e063b5871d9b5e81b19b82a82629e3ea23
arpanpathak <arpanpathak1996@gmail.com>
2020-10-04 00:26:35 +0530

    Added k8 directory for pods

--- core/controllers/controlers-list.go
+++ core/controllers/controlers-list.go
@@ -1,7 +1,6 @@
 // Register all your controller instances here
 package controllers
-
-
+ 
 // InjectControlelrs this method is used to inject all the registered controlelrs dynamically
 function InjectControllers() {
 	// TODO
