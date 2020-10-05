f891acb80cf08af88c98206b5b1b7176b119baee
arpanpathak <arpan.pathak47@gmail.com>
2020-10-06 04:09:38 +0530

    Middlewere entry created

--- core/controllers/controlers-list.go
+++ core/controllers/controlers-list.go
@@ -1,17 +1,21 @@
 // Register all your controller instances here
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/muSonorous-core/core/middlewere"
+)
 
 // ControllerEntity represents a Controlelr
 type ControllerEntity struct {
-	routeGroup     string
-	authMiddleWere string
-	routeName      string
-	method         string
-	handler        func(c *gin.Context)
+	routeGroup  string
+	middleweres []middlewere.Middlewere
+	routeName   string
+	method      string
+	handler     func(c *gin.Context)
 }
 
+// Array of all the available routes which needs to be registered
 var routes = []ControllerEntity{
-	ControllerEntity{"", "", "ping", "GET", Ping},
+	ControllerEntity{"", []middlewere.Middlewere{}, "ping", "GET", Ping},
 }
